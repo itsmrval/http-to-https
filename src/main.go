a45aaf14c35373a22e42d79824c4ae8ca2eb5db2
Valentin <valentin.puccetti@gmail.com>
2024-08-05 15:20:35 +0200

    feat(log) advanced logging and env variable for port

--- src/main.go
+++ src/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		targetURL := "https://" + r.Host + r.RequestURI
+		log.Printf("[REDIRECT] %s - %s (%s)", targetURL, r.RemoteAddr, r.UserAgent())
 		http.Redirect(w, r, targetURL, http.StatusMovedPermanently)
 	})
 
-	log.Println("Service running on :80")
-	if err := http.ListenAndServe(":80", nil); err != nil {
-		log.Fatalf("%v", err)
+	log.Printf("[INFO] Service running on :%s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("[ERROR] %v", err)
 	}
 }
